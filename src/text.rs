f58a157d8afe1bb6ba7af7d2154c4ab5f8d9cc2a
Ed Cragg <drq.11235@gmail.com>
2020-01-24 16:49:03 +0800

    Add byte to vector function and test

--- src/text.rs
+++ src/text.rs
@@ -7,6 +7,18 @@ pub fn get_char_image(c: char) {
     println!("{:?}", bytes);
 }
 
+fn byte_to_vec(b: u8) -> Vec<u8> {
+    let mut res = vec![];
+    for i in 0..8 {
+        if b & (1 << i) > 0 {
+            res.push(0);
+        } else {
+            res.push(255);
+        }
+    }
+    res
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -15,5 +27,17 @@ mod tests {
     fn test_get_char_image() {
         get_char_image('C');
         panic!("oh noes");
+    }
+
+    #[test]
+    fn test_byte_to_vec() {
+        assert_eq!(byte_to_vec(60),  [255, 255, 0,   0,   0,   0,   255, 255]);
+        assert_eq!(byte_to_vec(102), [255, 0,   0,   255, 255, 0,   0,   255]);
+        assert_eq!(byte_to_vec(3),   [0,   0,   255, 255, 255, 255, 255, 255]);
+        assert_eq!(byte_to_vec(3),   [0,   0,   255, 255, 255, 255, 255, 255]);
+        assert_eq!(byte_to_vec(3),   [0,   0,   255, 255, 255, 255, 255, 255]);
+        assert_eq!(byte_to_vec(102), [255, 0,   0,   255, 255, 0,   0,   255]);
+        assert_eq!(byte_to_vec(60),  [255, 255, 0,   0,   0,   0,   255, 255]);
+        assert_eq!(byte_to_vec(0),   [255, 255, 255, 255, 255, 255, 255, 255]);
     }
 }
