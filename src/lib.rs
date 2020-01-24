b952ca8b4c25e7ef715a2a5b5923c46e1daf93ff
Ed Cragg <drq.11235@gmail.com>
2020-01-24 15:25:55 +0800

    Start implementing SilyCaptcha type

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,60 @@
-fn main() {
-    println!("Hello, world!");
+extern crate font8x8;
+extern crate image;
+
+/// Struct used to configure and build a SillyCaptcha
+#[derive(Clone)]
+struct SillyCaptcha {
+    width: usize,
+    height: usize,
+    padding: usize,
+    num_chars: usize,
+    data: Vec<u8>,
+}
+
+impl Default for SillyCaptcha {
+    fn default() -> Self {
+        Self {
+            width: 100,
+            height: 50,
+            padding: 5,
+            num_chars: 4,
+            data: vec![],
+        }
+    }
+}
+
+impl SillyCaptcha {
+    /// Obtain an uninitialized SillyCaptcha
+    fn new() -> Self {
+        Self::default()
+    }
+
+    /// Set the width of the generated captcha image.
+    fn width(&mut self, width: usize) -> &mut Self {
+        self.width =  width;
+        self
+    }
+
+    /// Set the height of the generated captcha image.
+    fn height(&mut self, height: usize) -> &mut Self {
+        self.height =  height;
+        self
+    }
+
+    /// Set the padding of the generated captcha image.
+    fn padding(&mut self, padding: usize) -> &mut Self {
+        self.padding =  padding;
+        self
+    }
+
+    /// Set the num_chars of the generated captcha image.
+    fn num_chars(&mut self, num_chars: usize) -> &mut Self {
+        self.num_chars =  num_chars;
+        self
+    }
+
+    /// Initialise the SillyCaptcha struct
+    fn build(&mut self) -> Self {
+        self.clone()
+    }
 }
